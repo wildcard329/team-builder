9c35a6f2672c99adde06e143653d396ede97ed1e
wildcard329 <enkiduwildman@gmail.com>
2020-02-26 13:10:38 -0800

    initialized memberform

--- teambuilder2/src/components/MemberForm.js
+++ teambuilder2/src/components/MemberForm.js
@@ -1,7 +1,42 @@
-import React from 'react';
+import React, {useState} from 'react';
 
-export default function MemberForm() {
+ const MemberForm = props => {
+     const [member, setMember] = useState({
+         name: '',
+         email: '',
+         position: ''
+     })
+     const handleChanges = e => {
+         setMember({...member, [e.target.name]: e.target.value});
+     };
+     const submitForm = e => {
+         e.preventDefault();
+         props.addMember(member);
+     }
     return (
-        <></>
+        <Form onSubmit={submitForm}>
+            <label htmlFor="name">Member Name</label>
+            <input
+            id="name"
+            type="text"
+            name="name"
+            onChange = {handleChanges}
+            value={member.name} />
+            <label htmlFor="email">Member Email</label>
+            <input
+            id="email"
+            type="email"
+            name="email"
+            onChange={handleChanges}
+            value={member.email} />
+            <label htmlFor="position">Member Position</label>
+            <input
+            id="postion"
+            type="text"
+            name="position"
+            onChange={handleChanges}
+            value={member.position} />
+        </Form>
     )
 }
+export default MemberForm;
