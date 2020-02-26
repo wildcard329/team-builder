525fe30ed8b51a57b72f2adc1e46b26370661bdb
wildcard329 <enkiduwildman@gmail.com>
2020-02-26 13:15:19 -0800

    added member.js

--- teambuilder2/src/components/Member.js
+++ teambuilder2/src/components/Member.js
@@ -1,7 +1,15 @@
 import React from 'react';
 
-export default function Member() {
+const Member = props => {
     return (
-        <></>
+        <div className="member-list">
+            {props.member.mpa(mbr => (
+                <div className="member" key={mbr.id}>
+                    <p>{mbr.name}</p>
+                    <p>{mbr.email}</p>
+                    <p>{mbr.position}</p>
+                </div>
+            ))}
+        </div>
     )
 }
